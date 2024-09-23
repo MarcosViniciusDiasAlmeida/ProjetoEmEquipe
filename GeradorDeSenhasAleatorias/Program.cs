2df7e9f8c541ea4d74fba23044b0f81af058da2a
Vinicius <vinicius.paiva2@outlook.com>
2024-09-23 08:51:38 -0300

    Atividade03_Vinicius

---
+++ GeradorDeSenhasAleatorias/Program.cs
@@ -0,0 +1,15 @@
+﻿Random random = new Random();
+int lenght;
+string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+string senha = "";
+
+Console.Write("Digite o tamanho da senha (mín. 6 e máx. 16): ");
+while(!int.TryParse(Console.ReadLine(), out lenght) || lenght < 6 || lenght > 16){Console.Write("Digite um valor válido: ");}
+
+for(int i = 1; i <= lenght; i++)
+{
+    int ranNum = random.Next(0, chars.Length + 1);
+    senha += chars[ranNum];
+}
+
+Console.Write($"Senha Aleatória Gerada: {senha}");
