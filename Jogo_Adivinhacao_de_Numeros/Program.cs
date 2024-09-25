af02646b24733243d2800aecd82bc94b947873c6
Luiz Eduardo <luizinformaticaeduardo@gmail.com>
2024-09-25 10:02:40 -0300

    Codigo alinhado

--- Jogo_Adivinhacao_de_Numeros/Program.cs
+++ Jogo_Adivinhacao_de_Numeros/Program.cs
@@ -1,37 +1,37 @@
-﻿int valoraleat =0;
+﻿int valoraleat = 0;
 int total = 0;
 
 Random randNum = new Random();
-int num = randNum.Next(1,100);
+int num = randNum.Next(1, 100);
 
-while(num !=valoraleat)
+while (num != valoraleat)
 {
-    Console.ForegroundColor = ConsoleColor.Blue;    
+    Console.ForegroundColor = ConsoleColor.Blue;
     Console.Write("Adivinhe um número aleatório: ");
     Console.ResetColor();
-    if(int.TryParse(Console.ReadLine(),out valoraleat))
+    if (int.TryParse(Console.ReadLine(), out valoraleat))
 
-    if(valoraleat < num)
-    {
-        Console.ForegroundColor = ConsoleColor.DarkRed;
-        Console.WriteLine("Valor Acima!");
-        Console.ResetColor();
-        total++;
-    }
-    else if(valoraleat > num)
-    {
-        Console.ForegroundColor = ConsoleColor.DarkRed;
-        Console.WriteLine("Valor Abaixo!");
-        Console.ResetColor();
-        total++;
-    }
-    else
-    {
-        Console.ForegroundColor = ConsoleColor.DarkGreen;
-        Console.WriteLine("\nNúmero Certo!\n");
-        Console.ResetColor();
-        total++;
-    }
+        if (valoraleat < num)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Valor Acima!");
+            Console.ResetColor();
+            total++;
+        }
+        else if (valoraleat > num)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Valor Abaixo!");
+            Console.ResetColor();
+            total++;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\nNúmero Certo!\n");
+            Console.ResetColor();
+            total++;
+        }
 }
 Console.ForegroundColor = ConsoleColor.DarkGreen;
 Console.WriteLine($"O numero era {num}.");
--- Jogo_Adivinhacao_de_Numeros/obj/Debug/net8.0/Jogo_Advinhacao_de_Numeros.AssemblyInfo.cs
+++ Jogo_Adivinhacao_de_Numeros/obj/Debug/net8.0/Jogo_Advinhacao_de_Numeros.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jogo_Advinhacao_de_Numeros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2da420831eb3e653e8febf0091cc86167fd5307")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aac6e89ce0d14c205e2e74437642423cb9ef97c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jogo_Advinhacao_de_Numeros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jogo_Advinhacao_de_Numeros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
