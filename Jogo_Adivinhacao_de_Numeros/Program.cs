aac6e89ce0d14c205e2e74437642423cb9ef97c0
Luiz Eduardo <luizinformaticaeduardo@gmail.com>
2024-09-25 09:56:42 -0300

    Adicionado codigo Jogo_Advinhacao_de_Numeros

---
+++ Jogo_Adivinhacao_de_Numeros/Program.cs
@@ -0,0 +1,39 @@
+﻿int valoraleat =0;
+int total = 0;
+
+Random randNum = new Random();
+int num = randNum.Next(1,100);
+
+while(num !=valoraleat)
+{
+    Console.ForegroundColor = ConsoleColor.Blue;    
+    Console.Write("Adivinhe um número aleatório: ");
+    Console.ResetColor();
+    if(int.TryParse(Console.ReadLine(),out valoraleat))
+
+    if(valoraleat < num)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("Valor Acima!");
+        Console.ResetColor();
+        total++;
+    }
+    else if(valoraleat > num)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("Valor Abaixo!");
+        Console.ResetColor();
+        total++;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("\nNúmero Certo!\n");
+        Console.ResetColor();
+        total++;
+    }
+}
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine($"O numero era {num}.");
+Console.WriteLine($"A quantidade de tentativas foram {total}.\n");
+Console.ResetColor();
